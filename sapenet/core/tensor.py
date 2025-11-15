07d7337dc415d0099d135038b17630b3986a5ae6
CodingTrue <codingtrue@web.de>
2025-11-15 21:59:24 +0100

    Added size getter and made data optional for tensors

--- sapenet/core/tensor.py
+++ sapenet/core/tensor.py
@@ -28,11 +28,14 @@ class Tensor:
     FLOAT = np.float32
     INT = np.int32
 
-    def __init__(self, data: np.ndarray):
-        if data.dtype not in (Tensor.FLOAT, Tensor.INT): raise ValueError(f"Datatype '{data.dtype}' is not supported.")
+    def __init__(self, data: Optional[np.ndarray] = None):
+        if isinstance(data, np.ndarray) and data.dtype not in (Tensor.FLOAT, Tensor.INT): raise ValueError(f"Datatype '{data.dtype}' is not supported.")
 
         self._data = data
         self._compute_context = ComputeContext.constant()
+
+    def size(self):
+        return self._data.size if isinstance(self._data, np.ndarray) else -1
 
     def context(self) -> ComputeContext:
         return self._compute_context
@@ -43,7 +46,7 @@ class Tensor:
     def __truediv__(self, other): return _tensor_bin_op(a=self, b=other, operation=OperationType.DIVIDE)
 
 def _tensor_bin_op(a: Tensor, b: Tensor, operation: OperationType) -> Tensor:
-    t = Tensor(np.float32(-1))
+    t = Tensor()
 
     t._compute_context = ComputeContext(is_constant=False, left=a, right=b, operation=operation)
 
